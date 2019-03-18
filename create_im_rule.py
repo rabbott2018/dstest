8c75711c29142da53df0cd3c74658988461743db
ricka <rick_abbott@trendmicro.com>
2019-03-18 12:41:41 -0400

    updated create_im_rule.py and imrule.txt to include host and included files

--- create_im_rule.py
+++ create_im_rule.py
@@ -18,19 +18,21 @@ host,hostname = var0.split("=")
 print (hostname.rstrip())
 imrule, rulename = var1.split("=")
 print (rulename.rstrip())
-include, includedir = var2.split("=")
-print (includedir.rstrip())
-exclude, excludedir = var3.split("=")
-print (excludedir.rstrip())
+base, basedir = var2.split("=")
+print (basedir.rstrip())
+include, includefiles = var3.split("=")
+print (includefiles.rstrip())
+exclude, excludefiles = var4.split("=")
+print (excludefiles.rstrip())
 
 # Setup
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 configuration = deepsecurity.Configuration()
-configuration.host = 'https://rickadsquic-dsmelb-17fdwfok283md-1362197788.us-east-1.elb.amazonaws.com/api'
+configuration.host = 'https://ec2-35-171-228-214.compute-1.amazonaws.com:4119/api'
 
 # Authentication
-configuration.api_key['api-secret-key'] = '3:JrbkU4dZBTxHaMJZBi80Tqksprv24+7nXDLwIEUlSmU='
+configuration.api_key['api-secret-key'] = '6:ff9K3EtTWAh4qYUh2amGWNohOP6Pt2P+/PAg1hvKfsk='
 
 # Initialization
 # Set Any Required Values
@@ -38,8 +40,9 @@ api_instance = deepsecurity.IntegrityMonitoringRulesApi(deepsecurity.ApiClient(c
 api_version = 'v1'
 integrity_monitoring_rule = deepsecurity.IntegrityMonitoringRule()
 integrity_monitoring_rule.name=rulename.rstrip()
-integrity_monitoring_rule.file_base_directory=includedir.rstrip()
-integrity_monitoring_rule.file_excluded_values=excludedir.rstrip()
+integrity_monitoring_rule.file_base_directory=basedir.rstrip()
+integrity_monitoring_rule.file_included_values=includefiles.rstrip()
+integrity_monitoring_rule.file_excluded_values=excludefiles.rstrip()
 integrity_monitoring_rule.template="file"
 
 try:
